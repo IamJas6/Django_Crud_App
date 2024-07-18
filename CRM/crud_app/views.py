c3783ad817db638b0847ea783ccf68ab3d1da10c
iamjas <suhailuddin2002@gmail.com>
2024-07-19 01:35:03 +0530

    update 2

--- CRM/crud_app/views.py
+++ CRM/crud_app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
+def index(request):
     return render(request, 'crud_app/index.html')
+
+
+def login(request):
+    return render(request, 'crud_app/login.html')
+
+
+def register(request):
+    return render(request, 'crud_app/register.html')
+
+
+def dashboard(request):
+    return render(request, 'crud_app/dashboard.html')
+
+
+def logout(request):
+    pass
