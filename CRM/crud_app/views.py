3a58ac5168d38967cabad00b67503f7795a7ad4c
IamJas6 <suhailuddin2002@gmail.com>
2024-07-25 15:45:03 +0530

    Login View Added

--- CRM/crud_app/views.py
+++ CRM/crud_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, LoginForm
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import auth
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 # Create your views here.
@@ -25,12 +27,35 @@ def register(request):
 
 #User registration form handling
 def login(request):
-    return render(request, 'crud_app/login_page.html')
+    form = LoginForm()                                 #import the form in a variable
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                messages.success(request, 'You Are Now Logged In Successfully!')
+                return redirect('dashboard')
+            else:
+                messages.info(request, 'Username or Password is incorrect')
+                return redirect('login')
+        else:
+            messages.info(request, 'Username not registered, Register Now!')
+            return redirect('register')
+    context = {'form':form}
+    return render(request, 'crud_app/login_page.html', context)
 
 
+#Logout user
 def logout(request):
-    pass
+    auth.logout(request)
+    messages.success(request, 'You havve been logged out!')
+    return redirect('/')
 
 
+#user dashboard
+@login_required(login_url='login')
 def dashboard(request):
     return render(request, 'crud_app/dashboard.html')
