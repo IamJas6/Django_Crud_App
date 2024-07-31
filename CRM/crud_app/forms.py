23530e109f8fbb34825ca025c69e213dd40cba6e
IamJas6 <suhailuddin2002@gmail.com>
2024-07-31 23:42:10 +0530

    Create Record Form

--- CRM/crud_app/forms.py
+++ CRM/crud_app/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm # use
 from django.contrib.auth.models import User  # used for registering username and password
 from django.forms.widgets import PasswordInput, TextInput, EmailInput  # used for password and text input based on imported form
 from django import forms 
-
+from .models import CreateRecord
 
 class CustomUserCreationForm(UserCreationForm):     #Register or Create a user
     class Meta:
@@ -13,3 +13,19 @@ class CustomUserCreationForm(UserCreationForm):     #Register or Create a user
 class LoginForm(AuthenticationForm):    #Login a user ,   including authentication in the form
     username = forms.CharField(widget=TextInput())
     password = forms.CharField(widget=PasswordInput)
+
+
+#CRUD Operation forms
+
+#Create Record Form
+class CreateRecordForm(forms.ModelForm): 
+    class Meta:
+        model = CreateRecord
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'province', 'country']
+
+
+#Upodate Record Form
+class UpdateRecordForm(forms.ModelForm):
+    class Meta:
+        model = CreateRecord
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'province', 'country']
