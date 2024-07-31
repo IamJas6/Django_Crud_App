356bd7fffa6b16adf7c4c5e861b9ce344569dda1
IamJas6 <suhailuddin2002@gmail.com>
2024-07-31 23:43:43 +0530

    url for create record

--- CRM/crud_app/urls.py
+++ CRM/crud_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logout/', views.logout, name="logout"),
     path('register/', views.register, name="register"),
     path('dashboard/', views.dashboard, name="dashboard"),
+    path('create_record/', views.createrecord, name="createrecord"),
 ]
