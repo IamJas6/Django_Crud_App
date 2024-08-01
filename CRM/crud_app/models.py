9d3847665d784663348651c3c7e8ff30d294bc7d
IamJas6 <suhailuddin2002@gmail.com>
2024-08-01 16:25:30 +0530

    Latest Updates

--- CRM/crud_app/models.py
+++ CRM/crud_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class CreateRecord(models.Model):
-    date_create = models.DateField(auto_now_add=True)
+    date_create = models.DateTimeField(auto_now_add=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=255)
