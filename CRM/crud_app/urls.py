2ebf6d89f8f3e122aa03f6abec4888b5ef8cb9f1
IamJas6 <suhailuddin2002@gmail.com>
2024-08-01 16:23:57 +0530

    update and view url created

--- CRM/crud_app/urls.py
+++ CRM/crud_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('register/', views.register, name="register"),
     path('dashboard/', views.dashboard, name="dashboard"),
     path('create_record/', views.createrecord, name="createrecord"),
+    path('update_record/<int:pk>', views.updaterecord, name="updaterecord"),
+    path('view_record/<int:pk>', views.viewrecord, name="viewrecord"),
 ]
