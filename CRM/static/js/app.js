a27cea48c83982fe23d0f645e6638bbdc9246666
IamJas6 <suhailuddin2002@gmail.com>
2024-08-05 15:07:25 +0530

    message timer function

---
+++ CRM/static/js/app.js
@@ -0,0 +1,11 @@
+//Message or notification timer
+
+var message_timeout = document.getElementById("message-timer");
+setTimeout(function(){
+    message_timeout.style.display = "none";
+}, 3000);
+
+var message_timeout = document.getElementById("message-timers");
+setTimeout(function(){
+    message_timeout.style.display = "none";
+}, 3000);
